63dd4a71780557e748b6faa678906dd267023f79
BuiBao3103 <hongbao2003@gmail.com>
2022-12-23 17:37:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const wrapperContainer = document.querySelector("#wrapper .container");
+const inputForm = document.querySelector("#input")
+
+
+let generatorLineNumber = (start,end) => {
+  for (let i = start; i <= end; i++) {
+    let span = document.createElement("span");
+    let p = document.createElement("p");
+    p.innerHTML = i;
+    if (i > -1 && i < 10) p.style.transform = "translate(" + -100 + "%," + 25 + "px)";
+    else p.style.transform = "translate(" + -175 + "%," + 25 + "px)";
+    span.appendChild(p);
+    wrapperContainer.appendChild(span);
+  }
+  let spanContainer = document.querySelectorAll("#wrapper span");
+  if (spanContainer.length > 1) {
+    new LeaderLine(spanContainer[0], spanContainer[spanContainer.length - 1], {
+      startPlug: "behind",
+      endPlug: "behind",
+      color: "rgb(0,0,0)",
+      size: 2,
+    });
+  }
+};
+document.querySelector("#input button").addEventListener('click',()=>{
+  let start = parseInt(document.querySelector("#start").value)
+  let end = parseInt(document.querySelector("#end").value)
+  if(isNaN(start) || isNaN(end)){
+    alert("Điền đầy đủ Start và End!")
+    return
+  }
+  if(start >= end){
+    alert("Start phải bé hơn End!")
+    document.querySelector("#start").value = ""
+    document.querySelector("#end").value = ""
+    return
+  }
+  
+  inputForm.style.display = "none"
+  generatorLineNumber(start,end)
+})
+
+// generatorLineNumber();
